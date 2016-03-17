fd0736586db1e2b15f1903613cb21d8fcda9278c
anantalamichhane <anantalamichhane1@gmail.com>
2016-03-17 18:08:19 +0545

    comment added.

--- app/controllers/db_officer/main_controller.rb
+++ app/controllers/db_officer/main_controller.rb
@@ -3,7 +3,9 @@ require_dependency "db_officer/application_controller"
 module DbOfficer
   class MainController < ApplicationController
     def index
-
+      if params[:table_name]
+       @columns = ActiveRecord::Base.connection.columns(params[:table_name])
+      end
     end
   end
 end
